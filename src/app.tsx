e3a3b9f34338b6420c2db458f623a7d085f22df2
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2020-08-13 08:31:56 -0600

    Update news fetcher code, fix tab spacing (#2)

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,20 @@
 import { h, Fragment } from "preact";
-import Footer from "./components/footer"
-import Filler from "./components/filler"
-import News from "./components/news"
-import Learn from "./components/learn"
-import Top from "./components/top"
-import Header from "./components/header"
-
-var style = require("./style/main.module.css");
+import Footer from "./components/footer";
+import Filler from "./components/filler";
+import News from "./components/news";
+import Learn from "./components/learn";
+import Top from "./components/top";
+import Header from "./components/header";
 
 export function App() {
-  return (
-    <Fragment>
-      <Header />
-      <Top />
-      <Learn />
-      <News />
-      <Filler />
-      <Footer />
-    </Fragment>
-  )
+	return (
+		<Fragment>
+			<Header />
+			<Top />
+			<Learn />
+			<News />
+			<Filler />
+			<Footer />
+		</Fragment>
+	);
 }
--- src/components/news-fetcher.tsx
+++ src/components/news-fetcher.tsx
@@ -1,78 +1,90 @@
 import { h, Component } from "preact";
 const style = require("../style/news.module.css");
-import moment from "moment"
+import moment from "moment";
 
 export default class NewsFetcher extends Component<{}, any> {
-    constructor(props) {
-        super(props);
-        this.state = {
-                fill: [],
-                isLoaded: false
-            };
-        this.NewsFetcher = this.NewsFetcher.bind(this);
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			fill: [],
+			isLoaded: false,
+		};
+		this.NewsFetcher = this.NewsFetcher.bind(this);
+	}
 
-    componentDidMount() {
-        this.NewsFetcher();
-    }
+	componentDidMount() {
+		this.NewsFetcher();
+	}
 
-    async NewsFetcher() {
-        const response = await fetch('/resources/articles.json')
-        const json = await response.json()
-        const data = json.slice(Math.max(json.length - 4, 0))
+	async NewsFetcher() {
+		const response = await fetch('/resources/articles.json');
+		const json = await response.json();
+		const data = json.slice(Math.max(json.length - 4, 0));
 
-        this.setState({
-                fill: data,
-                isLoaded: true
-            })
-    }
+		this.setState({
+			fill: data,
+			isLoaded: true
+		});
+	}
 
-    render() {
-        if (!this.state.isLoaded) {
-            return(
-                <p>Loading...</p>
-            )
-        }
-        return(
-            <div className={style.articleCont}>
-                {
-                this.state.fill.map(function(post) {
-                    const auth = []
-                    const firstThree = auth.slice(0, 3);
-                    const tagList = []
+	render() {
+		if (!this.state.isLoaded) {
+			return (
+				<p>Loading...</p>
+			)
+		}
+		return (
+			<div className={style.articleCont}>
+				{
+				this.state.fill.map(post => {
+					const authors = [];
+					const tagList = [];
 
-                    for (var i = 0; i < post.contributors.authors.length; i++) {
-                        const authors = post.contributors.authors[i].name;
-                        auth.push(authors)
-                    }
-                    for (var i = 0; i < post.tags.length; i++) {
-                        const tags = post.tags[i];
-                        tagList.push(tags)
-                    }
+					for (const authorObj of post.contributors.authors) {
+						authors.push(authorObj.name);
+					}
+					for (const tag of post.tags) {
+						tagList.push(tag);
+					}
 
-                    function getAuths() {
-                        return (auth.length > 2 ? firstThree.join(", ") + ", and " + (auth.length - firstThree.length) + " more" : auth);
-                    }
+					function getAuthors() {
+						let auth;
+						switch (authors.length) {
+							case 1:
+								auth = authors[0];
+								break;
+							case 2:
+								auth = `${authors[0]} and ${authors[1]}`;
+								break;
+							case 3:
+								auth = (<span style="display:inline;">{authors.slice(0, 2).join(", ")}, and {authors.slice(2)[0]}</span>);
+								break;
+							default:
+								auth = (<span style="display:inline;">{authors.slice(0, 3).join(", ")}, and <abbr title={authors.slice(3).join(", ")}>{authors.slice(3).length} more</abbr></span>);
+								break;
+						}
+						return auth;
+					}
 
-                    function getTags() {
-                        const hashTag = tagList.map(i => "#" + i).join(" ");
-                        return (hashTag);
-                    }
+					function getTags() {
+						const hashTag = tagList.map(i => "#" + i).join(" ");
+						return hashTag;
+					}
 
-                    return(
-                        <div className={[style.articleCard, style.shadow].join(" ")}>
-                            <div className={style.pic}></div>
-                            <div>
-                                <span className={style.title}>{post.title}</span>
-                                <span className={style.auth}>By: {getAuths()}</span>
-                                <span className={style.date}>{moment(post.pubdate).format('MMM Do YYYY')}</span>
-                                <span className={style.tags}>{getTags()}</span>
-                            </div>
-                        </div>
-                    )
-                })
-                }
-            </div>
-        )
-    }
+					return (
+						<div className={[style.articleCard, style.shadow].join(" ")}>
+							<div className={style.pic}></div>
+							<div>
+								<span className={style.title}>{post.title}</span>
+								<span className={style.authors}>By {getAuthors()}</span>
+								<span className={style.date}>{moment(post.pubdate).format('MMM Do, YYYY')}</span>
+								<span className={style.tags}>{getTags()}</span>
+							</div>
+						</div>
+					);
+				})
+				}
+			</div>
+		)
+	}
 }
