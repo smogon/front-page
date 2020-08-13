ca1d6151bd6942906986c0e93b1d50d731fe6326
Kris Johnson <11083252+KrisXV@users.noreply.github.com>
2020-08-13 03:26:51 -0600

    Update news fetcher code, fix tab spacing

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,37 +1,37 @@
 import { h } from "preact";
-var style = require("../style/footer.module.css");
+const style = require("../style/footer.module.css");
 
 export default () => {
-    return (
-        <footer>
-            <div className={style.footerGrid}>
-                <div>
-                    <img src="https://www.smogon.com/media/zracknel-beta.svg.m.1" alt="Smogon" />
-                    <div className={style.uni}>
-                        <div>Smogon</div> 
-                        <div>University</div>
-                    </div>
-                </div>
+	return (
+		<footer>
+			<div className={style.footerGrid}>
+				<div>
+					<img src="https://www.smogon.com/media/zracknel-beta.svg.m.1" alt="Smogon" />
+					<div className={style.uni}>
+						<div>Smogon</div> 
+						<div>University</div>
+					</div>
+				</div>
 
-                <div>
-                    <ul>
-                        <li>About</li>
-                        <li>Pok&eacute;dex</li>
-                        <li>Forums</li>
-                        <li>Tournaments</li>
-                        <li>Pok&eacute;mon Showdown!</li>
-                    </ul>
+				<div>
+					<ul>
+						<li>About</li>
+						<li>Pok&eacute;dex</li>
+						<li>Forums</li>
+						<li>Tournaments</li>
+						<li>Pok&eacute;mon Showdown!</li>
+					</ul>
 
-                    <div className={style.social}>
-                        <i class="fab fa-twitter fa-4x"></i>
-                        <i class="fab fa-facebook-f fa-4x"></i>
-                        <i class="fab fa-youtube fa-4x"></i>
-                        <i class="fab fa-discord fa-4x"></i>
-                    </div>
+					<div className={style.social}>
+						<i class="fab fa-twitter fa-4x"></i>
+						<i class="fab fa-facebook-f fa-4x"></i>
+						<i class="fab fa-youtube fa-4x"></i>
+						<i class="fab fa-discord fa-4x"></i>
+					</div>
 
-                    <div className={style.copy}>All guides and strategy information are &copy; 2004-2020 Smogon.com and its contributors. Pokémon is &copy; 1995-2020 Nintendo. Privacy Policy</div>
-                </div>
-            </div>
-        </footer>
-    );
+					<div className={style.copy}>All guides and strategy information are &copy; 2004-2020 Smogon.com and its contributors. Pok&eacute;mon is &copy; 1995-2020 Nintendo. Privacy Policy</div>
+				</div>
+			</div>
+		</footer>
+	);
 };
